3088157204dde8d648232e951c7afd0be66e5c7d
Moe Boe <modiomar99@gmail.com>
2022-07-20 00:08:08 +0300

    updating

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {createRoot} from 'react-dom/client';
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons'; 
 
+createRoot(document.getElementById('root')).render(<App />);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
 // registerServiceWorker();
